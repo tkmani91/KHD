75f8a34f64ae38282c7333880e25465b67c952a1
TANMOY MANI <97333526+tkmani91@users.noreply.github.com>
2026-03-10 00:23:41 +0600

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@
 const Header = () => {
   return (
     <header style={{
-      background: 'linear-gradient(135deg, ##ffc145 0%, #D32F2F 100%)',
+      background: 'linear-gradient(135deg, #ffc145 0%, #D32F2F 100%)',
       padding: '10px 0',
       textAlign: 'center',
       boxShadow: '0 4px 15px rgba(0,0,0,0.3)',
