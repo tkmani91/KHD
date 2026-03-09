063291fe80ca7d31382ff50bcec8b92910d07d15
TANMOY MANI <97333526+tkmani91@users.noreply.github.com>
2026-03-10 00:27:14 +0600

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,10 +3,10 @@
 const Header = () => {
   return (
     <header style={{
-      background: 'linear-gradient(135deg, #ffc145 0%, #D32F2F 100%)',
-      padding: '10px 0',
-      textAlign: 'center',
-      boxShadow: '0 4px 15px rgba(0,0,0,0.3)',
+     background: 'linear-gradient(135deg, #FFD700 0%, #E6B800 50%, #DAA520 100%)',
+  padding: '10px 0',
+  textAlign: 'center',
+  boxShadow: '0 4px 15px rgba(0,0,0,0.3)',
     }}>
       <img 
         src="https://raw.githubusercontent.com/tkmani91/Dharmasaba/main/hader%20Banner.png"
