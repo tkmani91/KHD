edadf88ba4175569f375167b379f3ef99cf426b5
TANMOY MANI <97333526+tkmani91@users.noreply.github.com>
2026-03-10 00:22:05 +0600

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,13 +3,13 @@
 const Header = () => {
   return (
     <header style={{
-      background: 'linear-gradient(135deg, #FF6B35 0%, #D32F2F 100%)',
+      background: 'linear-gradient(135deg, ##ffc145 0%, #D32F2F 100%)',
       padding: '10px 0',
       textAlign: 'center',
       boxShadow: '0 4px 15px rgba(0,0,0,0.3)',
     }}>
       <img 
-        src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhUTK5wFPPaWLXOn09yxkXaKCPpRuNnpm44PjDRaQCVsK_R1pLn4hoNFtApFktvJ77o-ItLiOh8mCkmVQU37DssgyYDdRmtyjWz19h8o4p78Rb_WyT09EsNVu6svOI_9La2pBeHlp7-VXg/s1600/KHDS3.png"
+        src="https://raw.githubusercontent.com/tkmani91/Dharmasaba/main/hader%20Banner.png"
         alt="কলম হিন্দু ধর্মসভা"
         style={{
           maxWidth: '100%',
