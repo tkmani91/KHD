0ba6d9a6ac63cb548148ca6bb3c274016e643bda
TANMOY MANI <97333526+tkmani91@users.noreply.github.com>
2026-03-22 22:07:42 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path" // এটি যোগ করুন
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -5,6 +6,11 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"), // এই অংশটুকু যোগ করা হয়েছে
+    },
+  },
   build: {
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
